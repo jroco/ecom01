f5d78552911d7395b1369a9ac9f718b15cd7d8e0
jroco <jonathanroco@gmail.com>
2021-06-14 00:14:50 -0400

    Creation fro home and about pages (2nd to push)

---
+++ portal01/blog/views.py
@@ -0,0 +1,10 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('<h1>Home page</h1>')
+
+def about(request):
+    return HttpResponse('<h1>About</h1>')
